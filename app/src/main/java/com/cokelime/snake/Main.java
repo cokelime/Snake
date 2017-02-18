b47cfe70e5be96d0fe4ee2ba86a8eededccd875e
Yichi.Chen <jack10401@gmail.com>
2017-02-18 14:41:48 -0500

    test commit

--- app/src/main/java/com/cokelime/snake/Main.java
+++ app/src/main/java/com/cokelime/snake/Main.java
@@ -1,7 +1,13 @@
 package com.cokelime.snake;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.graphics.Paint;
+import android.widget.RelativeLayout;
+
+import static com.cokelime.snake.R.id.activity_main;
 
 public class Main extends Activity {
 
@@ -9,5 +15,16 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Canvas canvas = new Canvas();
+
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawPaint(paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(20);
+        canvas.drawText("Some Text", 10, 25, paint);
     }
 }
