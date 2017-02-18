c65a4802b7387b3c8139cdbf13e5b24b9297b4b5
Cokelime <yeisan42@gmail.com>
2017-02-18 11:54:40 -0800

    remove test comments

--- app/src/main/java/com/cokelime/snake/Main.java
+++ app/src/main/java/com/cokelime/snake/Main.java
@@ -9,11 +9,5 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
-        //rem
-
-        //test2
-
-        //test # 3
     }
 }
