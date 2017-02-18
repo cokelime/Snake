57456781cf5d14c26d3ada0f1a87135b29f8485a
Yichi.Chen <jack10401@gmail.com>
2017-02-18 14:59:33 -0500

    Add view class

---
+++ app/src/main/java/com/cokelime/snake/View.java
@@ -0,0 +1,14 @@
+package com.cokelime.snake;
+
+import android.content.Context;
+
+/**
+ * Created by Yichi.Chen on 2/18/2017.
+ */
+
+public class View extends android.view.View {
+
+    public View(Context context) {
+        super(context);
+    }
+}
