4f03608a7060bf78767868da427b208ddb070ffc
Cokelime <yeisan42@gmail.com>
2017-02-18 11:02:17 -0800

    initial commit

---
+++ app/src/main/java/com/cokelime/snake/Main.java
@@ -0,0 +1,13 @@
+package com.cokelime.snake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
