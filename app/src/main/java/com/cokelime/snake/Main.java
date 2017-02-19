7e9f100f83d1051344485c1f408750f9cd3602d4
Cokelime <yeisan42@gmail.com>
2017-02-19 11:50:00 -0800

    add rectangle and move right method

--- app/src/main/java/com/cokelime/snake/Main.java
+++ app/src/main/java/com/cokelime/snake/Main.java
@@ -2,12 +2,28 @@ package com.cokelime.snake;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
+
+    private SnakeGrid grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        grid = (SnakeGrid) findViewById(R.id.grid);
+
+        Button rightButton = (Button) findViewById(R.id.rightButton);
+
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grid.moveRight(5);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/cokelime/snake/SnakeGrid.java
+++ app/src/main/java/com/cokelime/snake/SnakeGrid.java
@@ -1,6 +1,10 @@
 package com.cokelime.snake;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -9,7 +13,50 @@ import android.view.View;
 
 public class SnakeGrid extends View {
 
+    Rect rect;
+
+    private int xMax;
+    private int yMax;
+
+    private int rectScaleX;
+    private int rectScaleY;
+
+    private int rectX = 0;
+    private int rectY = 0;
+
+    Paint paint;
+
     public SnakeGrid(Context context) {
         super(context);
+        rect =  new Rect();
+        paint = new Paint();
+
+        paint.setColor(Color.BLACK);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        rect.set(rectX , rectY, rectX + rectScaleX, rectY + rectScaleY);
+
+        canvas.drawRect(rect, paint);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        xMax = w-1;
+        yMax = h-1;
+
+        rectScaleX = xMax / 8;
+        rectScaleY = yMax / 8;
+    }
+
+    public void moveRight(int amount){
+
+        rectX += amount;
+
+        invalidate();
     }
 }
